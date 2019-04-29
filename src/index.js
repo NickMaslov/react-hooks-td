86c3397b0040457c0d68c3e463a0a0c88b816d90
Mykola Maslov <kategorychnost@gmail.com>
2019-04-29 19:27:06 -0400

    Creating TodoList  using useContext and useReducer, styling it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 /** heact-hooks-intro */
 // import App from './AppClass';
 // import App from './AppFunction';
@@ -9,18 +10,35 @@ import ReactDOM from 'react-dom';
 /** hooks-news */
 // import App from './AppNews';
 
+
+/** hooks-reducer-counter */
+// import App from './AppCounterReducer';
+// export const UserContext = React.createContext()
+// const username = "Dave";
+//ReactDOM.render(<UserContext.Provider value={username}><App /></UserContext.Provider>, document.getElementById('root'));
+
+
 /** hooks-todos */
-import App from './AppCounterReducer';
-export const UserContext = React.createContext()
-const username = "Dave";
-//<App /> --> 
+
+import TodosContext from './context'
+import todosReducer from './reducer'
+import { useContext, useReducer} from 'react'
+import TodoList from './components/TodoList'
+import './AppNews.css'
+const App=()=>{
+    const initialState=useContext(TodosContext)
+    const [state, dispatch] = useReducer(todosReducer, initialState)
+    
+    return (
+    <TodosContext.Provider value={{state,dispatch}}>
+        <TodoList />
+    </TodosContext.Provider>)
+}
 
 
 
 
-// import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<UserContext.Provider value={username}><App /></UserContext.Provider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 /** for developmode styling/ hot reload */
 // if (module.hot) {
@@ -31,4 +49,4 @@ ReactDOM.render(<UserContext.Provider value={username}><App /></UserContext.Prov
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+serviceWorker.unregister();
